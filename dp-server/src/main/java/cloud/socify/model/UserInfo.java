35f4b0290b9cebab15f783b5a96e932cb6a0716f
0xFaulty <valenteen3d@yandex.ru>
2019-06-09 10:04:35 +0300

    #add soft skill set

--- dp-server/src/main/java/cloud/socify/model/UserInfo.java
+++ dp-server/src/main/java/cloud/socify/model/UserInfo.java
@@ -1,5 +1,6 @@
 package cloud.socify.model;
 
+import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,12 @@ import lombok.Setter;
 @AllArgsConstructor
 public class UserInfo {
 
+    private String email;
+    private String userType;
     private String firstName;
     private String secondName;
     private String middleName;
+    private String photo;
+    private String gender;
+    private Date birthday;
 }
--- dp-server/src/main/resources/db/migrations/mysql.sql
+++ dp-server/src/main/resources/db/migrations/mysql.sql
@@ -48,9 +48,10 @@ CREATE TABLE vacancy_skill
   skillId   BIGINT
 );
 
-CREATE TABLE education
+CREATE TABLE institution
 (
   id          BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  userId      BIGINT,
   name        TEXT                  NOT NULL,
   rating      DOUBLE                NOT NULL,
   description TEXT                  NOT NULL
@@ -58,25 +59,17 @@ CREATE TABLE education
 
 CREATE TABLE education_program
 (
-  id          BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  educationId BIGINT                NOT NULL,
-  name        TEXT                  NOT NULL,
-  rating      DOUBLE                NOT NULL,
-  description TEXT                  NOT NULL
-);
-
-CREATE TABLE education_faculty
-(
-  id          BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  educationId BIGINT                NOT NULL,
-  name        TEXT                  NOT NULL,
-  rating      TEXT                  NOT NULL,
-  description TEXT                  NOT NULL
+  id            BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  institutionId BIGINT                NOT NULL,
+  name          TEXT                  NOT NULL,
+  rating        DOUBLE                NOT NULL,
+  description   TEXT                  NOT NULL
 );
 
 CREATE TABLE company
 (
   id          BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  userId      BIGINT,
   name        TEXT                  NOT NULL,
   rating      DOUBLE                NOT NULL,
   description TEXT                  NOT NULL
@@ -85,11 +78,17 @@ CREATE TABLE company
 CREATE TABLE company_vacancy
 (
   companyId    BIGINT,
-  id           BIGINT,
+  vacancyId    BIGINT,
   name         TEXT NOT NULL,
   salary       TEXT,
   currencyType TEXT,
   description  TEXT NOT NULL
+);
+
+CREATE TABLE user_type
+(
+  id   BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  name TEXT                  NOT NULL
 );
 
 CREATE TABLE user
@@ -97,17 +96,28 @@ CREATE TABLE user
   id          BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   password    TEXT                  NOT NULL,
   email       TEXT                  NOT NULL,
-  user_type   VARCHAR(255)          NOT NULL,
-  banned      BOOLEAN               NOT NULL,
+  userType    TEXT                  NOT NULL,
+  banned      BOOLEAN               NOT NULL DEFAULT FALSE,
   createdAt   TIMESTAMP             NOT NULL DEFAULT NOW(),
   lastVisitAt TIMESTAMP             NOT NULL DEFAULT NOW()
 );
 
+CREATE TABLE student_info
+(
+  userId     BIGINT NOT NULL,
+  firstName  TEXT   NOT NULL,
+  secondName TEXT   NOT NULL,
+  middleName TEXT   NOT NULL,
+  photo      TEXT   NOT NULL,
+  gender     TEXT   NOT NULL,
+  birthday   DATE   NOT NULL
+);
+
 CREATE TABLE user_event
 (
-  userId   BIGINT NOT NULL,
-  eventId  TEXT   NOT NULL,
-  rating   DOUBLE NOT NULL
+  userId  BIGINT NOT NULL,
+  eventId TEXT   NOT NULL,
+  rating  DOUBLE NOT NULL
 );
 
 CREATE TABLE user_metric
@@ -116,32 +126,3 @@ CREATE TABLE user_metric
   metricId INTEGER NOT NULL,
   value    DOUBLE  NOT NULL
 );
-#
-# INSERT INTO event_type (id, name)
-# VALUES ('university program'),
-#        ('college program'),
-#        ('olympiad'),
-#        ('courses');
-
-# INSERT INTO event (typeId, name, description)
-# VALUES (3, 'Java', 'Java courses'),
-#        (3, 'SQL', 'SQL courses'),
-#        (0, 'Biology', 'Biology university program'),
-#        (1, 'Java', 'Biology college program'),
-#        (3, 'Java course work 1', 'simple course work');
-# INSERT INTO user_info (userId, firstName, secondName, middleName)
-# VALUES (2, 'Valentin', 'Bashkirov', 'Victorovich');
-#
-# INSERT INTO user_identity (userId, idNumber, idAddress)
-# VALUES (2, '556661116', 'SPb yl. Zamshina');
-#
-# INSERT INTO user_event (userId, eventId, rating)
-# VALUES (2, 0, 0.8);
-#
-# INSERT INTO company (name, rating, description)
-# VALUES ('Google'),
-#  ('Yandex'),
-#  ('JetBrains'),
-#  ('Faulty inc.');
-
-
